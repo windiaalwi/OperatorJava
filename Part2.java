9751f6af3438f5391b6429714dfc4ca0c4f69e6c
windiaalwi <141600480+windiaalwi@users.noreply.github.com>
2023-12-14 01:03:43 +0700

    Add files via upload

---
+++ Part2.java
@@ -0,0 +1,18 @@
+
+package javaarray;
+
+
+public class Part2 {
+    public static void main(String[] args) {
+        
+        String [] nama = {"toni","ina","dini","hasan"};
+        String [] kelas = {"X","XI","XII","X"};
+        String [] alamat = {"mlg","sby","mjk","jkt"};
+        
+        for ( int a = 1; a < nama.length; a++){
+            System.out.println(nama [a]);
+            System.out.println(kelas [a]);
+            System.out.println(alamat [a]);
+    }
+}
+}
