3d1c08e0dbb8692a9d23e203ced9113d65aea725
windiaalwi <141600480+windiaalwi@users.noreply.github.com>
2023-12-14 00:50:01 +0700

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,17 @@
+
+package latihanpercabangan;
+public class SwitchCase {
+    public static void main(String[] args) {
+        int day = 6;
+        
+    switch (day){
+        case 1 -> System.out.println("Mounday");
+        case 2 -> System.out.println("Tuesday");
+        case 3 -> System.out.println("Wednesday");
+        case 4 -> System.out.println("Thursday");
+        case 5 -> System.out.println("Friday");
+        case 6 -> System.out.println("Saturday");
+        case 7 -> System.out.println("Sunday"); 
+    }
+}
+}
