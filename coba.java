972d7609509f6e8c2804e621707f6971e6f95848
windiaalwi <141600480+windiaalwi@users.noreply.github.com>
2023-12-14 00:45:35 +0700

    Add files via upload

---
+++ coba.java
@@ -0,0 +1,23 @@
+
+public class coba {
+    public static void main(String[] args) {
+        String namaSiswa;
+        String namaSiswa2 = "jaehyun";
+        char jenisKelamin = 'l';
+        int usia = 20;
+        double tinggiBadan = 170.5;
+        float beratBadan = 50.6f;
+        boolean siswa = true;
+        final double phi = 3.14; 
+        
+       
+        System.out.println("nama siswa: "+ namaSiswa2); 
+       
+        
+        System.out.println("jenis kelamin: "+jenisKelamin);
+        System.out.println("========BIODATA SISWA========");
+    }
+}
+
+
+
