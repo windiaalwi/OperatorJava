8d4f0370fb4c88d16dd21358feeee872357cdf06
windiaalwi <141600480+windiaalwi@users.noreply.github.com>
2023-12-14 00:47:12 +0700

    Add files via upload

---
+++ OperatorAritmatika.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class OperatorAritmatika {
+
+    public static void main(String[] args) {
+
+        int bilangan1 = 13;
+        int bilangan2 = 5;
+        int hasilPenjumlahan = bilangan1 + bilangan2;
+        int hasilPengurangan = bilangan1 - bilangan2;
+        int hasilPembagian = bilangan1 / bilangan2;
+        int hasilPerkalian = bilangan1 * bilangan2;
+        int hasilModulus = bilangan1 % bilangan2;
+       
+        System.out.println("Hasil Penjumlahan:" + hasilPenjumlahan);
+        System.out.println("Hasil Pengurangan:" + hasilPengurangan);
+        System.out.println("HasiPembagian:" + hasilPembagian);
+        System.out.println("Hasil Perkalian:" + hasilPerkalian);
+        System.out.println("Hasil Sisa Bagi Hasil:" + hasilModulus);
+         
+
+        double nilaiMat = 80.9;
+        double nilaiProduktif = 99.89;
+        double nilaiInformatika = 95.78;
+        double nilaiPkn = 94.90;
+
+        double nilaiRata
+                = (nilaiMat + nilaiProduktif + nilaiInformatika + nilaiPkn) / 4;
+
+        System.out.println("Nilai rata-rata:" + nilaiRata);
+         
+         
+         
+        double beratBadan = 63;
+        double tinggiBadan = 180;
+        double hasilBeratbadan = beratBadan / tinggiBadan * 2;
+        System.out.println("Hasil Pembagian:" + hasilBeratbadan);
+        
+
+
+
+         
+         
+         
+  
+         
+       
+       
+     }
+     
+   
+        
+    }
+    
+
