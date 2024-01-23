31abd4b33c0135a190d725c8db928a0389cee9f5
windiaalwi <141600480+windiaalwi@users.noreply.github.com>
2024-01-23 19:47:32 +0700

    Update LycheeTea.java

--- LycheeTea.java
+++ LycheeTea.java
@@ -1,6 +1,13 @@
-
 package polymorphism;
 
-public class LycheeTea {
+public class LycheeTea extends minuman{
     
+    @Override
+    public void tampilJenis (String jenis){
+        System.out.println("Jenis minuman: "+jenis);
+    }
+    @Override
+    public int jenisMinuman(){
+        return super.jenisMinuman();
+    }
 }
